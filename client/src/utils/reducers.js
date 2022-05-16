e9e748bf9f208151594fdadfc1a39f8e996efce3
laszlo-ratesic <keenanrc@hotmail.com>
2022-05-16 12:25:15 -0500

    implement Add to Cart reducers with tests

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,6 +2,12 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from "./actions";
 
 import { useReducer } from "react";
@@ -27,11 +33,24 @@ export const reducer = (state, action) => {
         currentCategory: action.currentCategory,
       };
 
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
     default:
       return state;
   }
 };
 
 export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
+  return useReducer(reducer, initialState);
 }
