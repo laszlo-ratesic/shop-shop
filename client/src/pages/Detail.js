37708b21206da9735cd59cab97cc33fb8d11c086
laszlo-ratesic <keenanrc@hotmail.com>
2022-05-16 14:57:17 -0500

    handle cart quantity add and remove

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,15 +14,26 @@ import Cart from "../components/Cart";
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
-  const { id } = useParams();
   const [currentProduct, setCurrentProduct] = useState({});
+  const { products, cart } = state;
+  const { id } = useParams();
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  const { products } = state;
+
   const addToCart = () => {
-    dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 },
-    });
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+    }
   };
 
   useEffect(() => {
@@ -35,6 +46,13 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
+    });
+  };
 
   return (
     <>
@@ -49,7 +67,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
